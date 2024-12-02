4c8774f01f04bce8d9667c321666e3bf1b0fc437
PiotrZurynski <pewdiejanusz@gmail.com>
2024-12-02 08:46:18 +0100

    more added things

--- blackjack.py
+++ blackjack.py
@@ -1,11 +1,17 @@
 import random
 class BlackJack:
     listakart=[2,3,4,5,6,7,8,9,10,"J","Q","K","A"]
+    handValue=0
     def __init__(self):
         pass
     def hand(self):
         self.dealhand = random.sample(self.listakart, 2)
-        return self.dealhand
+        if self.dealhand[0]=="J" or "Q" or "K" and self.dealhand[1] =="J" or "Q" or "K":
+            self.handValue=10 
+        elif self.dealhand[0]=="A" and self.dealhand[1]=="A":
+            self.handValue=11
+        self.handValue=self.dealhand[0]+self.dealhand[1]
+        return self.dealhand 
     def hit(self):
          new_card=random.sample(self.listakart,1)
          self.dealhand.extend(new_card)
